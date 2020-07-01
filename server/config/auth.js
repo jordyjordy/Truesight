262110f12761993d2f574332cc1d834fd12a60f5
Jordy van der Tang <j.vandertang@student.tudelft.nl>
2020-07-01 20:17:54 +0200

    add token timeout functionality

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,15 +1,25 @@
 const jwt = require("jsonwebtoken")
+const User = require("../model/user")
 module.exports = async (req, res, next) => {
     try  {
         console.log("VALIDATING")
         const token = req.headers.token
         const decoded = await jwt.verify(token, "secret");
-        req.userData = decoded;
-        console.log("VALIDATED")
-        next()
+        const secondsSinceEpoch = Math.round(Date.now() / 1000)  
+        if(secondsSinceEpoch - decoded.iat >= 604800) {
+            console.log("Connection no longer valid")
+            //remove the token from the database anyway
+            await User.removeToken(decoded.email, req.headers.token)
+            //inform the client the connection has timed out
+            res.status(401).json({message:"Connection timed out"})
+        } else {
+            req.userData = decoded;
+            console.log("VALIDATED")
+            next()
+        }
     } catch (err) {
         console.log("VALIDATION FAILED")
-        return res.status(401).json({
+        res.status(401).json({
             message: "Authentication Failed"
         })
     }
