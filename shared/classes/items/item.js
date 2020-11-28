ff88b4810d227e4cac4c42646ca2ce6c5399bcc8
Jordy van der Tang <j.vandertang@student.tudelft.nl>
2020-11-28 11:06:09 +0100

    fix items not being able to be 0

--- shared/classes/items/item.js
+++ shared/classes/items/item.js
@@ -1,6 +1,6 @@
-class Item{
-    constructor(item){
-        
+class Item {
+    constructor(item) {
+
         this.class = 'item'
         this.name = item.name
         this.type = item.type
@@ -9,18 +9,18 @@ class Item{
         this.description = item.description
         this.icon = item.icon
         this.color = item.color
-        this.count = item.count?item.count:1
+        this.count = item.count !== undefined ? item.count : 0
         this._id = item._id
     }
-    display(){
+    display() {
         return {}
     }
 
     equals(item) {
         return this.name == item.name && this.type == item.type &&
-        this.cost == item.cost && this.weight == item.weight &&
-        this.description == item.description
-    } 
+            this.cost == item.cost && this.weight == item.weight &&
+            this.description == item.description
+    }
     static from(json) {
         return Object.assign(new Item(), json)
     }
